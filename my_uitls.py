61d4f44e6287fbeaeb336ed9067329b6f9079a58
Amit JS <amitjslearn@gmail.com>
2020-07-26 18:08:53 +0530

    Update my_uitls.py

--- my_uitls.py
+++ my_uitls.py
@@ -1,8 +1,12 @@
 import shutil
 
-def copy_arr(src_paths_arr:str, dest_folder:str):
+def copy_arr(src_paths_arr:str, dest_folder:str, prefix:str='')->None:
     for s in src_paths_arr:
         dfn = s.split('/')[-1]
+        if prefix:
+          ext, dfn = dfn[::-1].split('.',maxsplit=1)
+          ext, dfn = ext[::-1], dfn[::-1]
+          dfn = f'{prefix}_{dfn}.{ext}'
         shutil.copy(
             s,
             f'{dest_folder}/{dfn}'
